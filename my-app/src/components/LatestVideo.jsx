fa24d285e6fcc2df4c2f7675ad5ef13031db4c23
Allison Karp <akarp@mit.edu>
2026-04-01 19:08:52 -0400

    fixed api key to work hopefully

--- my-app/src/components/LatestVideo.jsx
+++ my-app/src/components/LatestVideo.jsx
@@ -4,20 +4,22 @@ export default function LatestVideo() {
   const [videoId, setVideoId] = useState(null);
 
   useEffect(() => {
-    const API_KEY = process.env.REACT_APP_YT_API_KEY;
-    const CHANNEL_ID = process.env.REACT_APP_YT_CHANNEL_ID;
+  const API_KEY = process.env.REACT_APP_YT_API_KEY;
+  const CHANNEL_ID = process.env.REACT_APP_YT_CHANNEL_ID;
 
-    fetch(
-      `https://www.googleapis.com/youtube/v3/search?key=${API_KEY}&channelId=${CHANNEL_ID}&part=snippet&order=date&maxResults=1&type=video`
-    )
-      .then(res => res.json())
-      .then(data => {
-        const latestVideo = data.items?.[0]?.id?.videoId;
-        if (latestVideo) {
-          setVideoId(latestVideo);
-        }
-      })
-      .catch(err => console.error("YouTube API error:", err));
+  const url = `https://www.googleapis.com/youtube/v3/search?key=${API_KEY}&channelId=${CHANNEL_ID}&part=snippet&order=date&maxResults=1&type=video`;
+  console.log("Fetching:", url); 
+
+  fetch(url)
+    .then(res => res.json())
+    .then(data => {
+      console.log("YouTube response:", data); 
+      const latestVideo = data.items?.[0]?.id?.videoId;
+      if (latestVideo) {
+        setVideoId(latestVideo);
+      }
+    })
+    .catch(err => console.error("YouTube API error:", err));
   }, []);
 
   return (
